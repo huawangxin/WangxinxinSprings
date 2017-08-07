bb29b26872f1d7252cfa7b88d945dc25d6b02a1e
huawangxin <huawangxin2015@qq.com>
2017-08-08 07:47:32 +0800

    更新代码

--- SpringMVC/WangxinSpringMVC1/src/com/wangxinxin/controller/HomeControllerTest.java
+++ SpringMVC/WangxinSpringMVC1/src/com/wangxinxin/controller/HomeControllerTest.java
@@ -18,13 +18,25 @@ import com.wangxinxin.service.SpitterService;
 public class HomeControllerTest {
 	@Test
 	public void shouldDisplayRecentSpittles(){
+		/**
+		 * 模拟SpitterService
+		 */
 		List<Spitter> expectedSpittles = asList(new Spitter(),new Spitter(),new Spitter());
 		SpitterService spitterService = mock(SpitterService.class);	
 		when(spitterService.getRecentSpittles(HomeController.DEFAULT_SPITTLES_PER_PAGE)).
 				thenReturn(expectedSpittles);
+		/**
+		 * 创建控制器
+		 */
 		HomeController controller=new HomeController(spitterService);
 		HashMap<String, Object> model=new HashMap<String,Object>();
+		/**
+		 * 调用处理方法
+		 */
 		String viewName= controller.showHomePage(model);
+		/**
+		 * 对结果进行断言
+		 */
 		assertEquals("home",viewName);
 		assertSame(expectedSpittles,model.get("spittles"));
 		verify(spitterService).getRecentSpittles(HomeController.DEFAULT_SPITTLES_PER_PAGE);
